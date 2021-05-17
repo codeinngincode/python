49100c742b79ad5a9d24993bf0d2fa21b25cd447
codeinngincode <66986522+codeinngincode@users.noreply.github.com>
2021-05-17 10:24:10 +0530

    Update and rename yusord to yusord.py

--- yusord.py
+++ yusord.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun May 16 11:24:03 2021
-
 @author: Fatima
 """
 from tkinter import *
@@ -13,26 +12,22 @@ root.title("yusuky")
 root.geometry("400x300")
 wL=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 print("sorry but i did not understant what text paramerter meant so it will be in console but you still had to press the button")
-random_alpha1=1
-random_alpha2=1
-random_alpha3=1
-random_alpha4=1
-random_alpha5=1
+
 
 
 
 def rl():
-    raN1=random.randint(0,25)
-    raN2=random.randint(0,25)
-    raN3=random.randint(0,25)
-    raN4=random.randint(0,25)
-    raN5=random.randint(0,25)
-    random_alpha1[rN1]
-    random_alpha2[rN2]
-    random_alpha3[rN3]
-    random_alpha4[rN4]
-    random_alpha5[rN5]
-    rlf=wL[rn]
+    rN1=random.randint(0,25)
+    rN2=random.randint(0,25)
+    rN3=random.randint(0,25)
+    rN4=random.randint(0,25)
+    rN5=random.randint(0,25)
+    random_alpha1=wL[rN1]
+    random_alpha2=wL[rN2]
+    random_alpha3=wL[rN3]
+    random_alpha4=wL[rN4]
+    random_alpha5=wL[rN5]
+    rlf=random_alpha1+random_alpha2+random_alpha3+random_alpha4+random_alpha5
     print("your lucky word is : "+rlf)
 
     
