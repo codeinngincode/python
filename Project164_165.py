8537d8883160ee962f981a150d522a45ec1e876a
codeinngincode <66986522+codeinngincode@users.noreply.github.com>
2021-06-24 14:58:30 +0530

    Update Project164_165.py

--- Project164_165.py
+++ Project164_165.py
@@ -12,11 +12,20 @@ label_image.place(relx=0.5, rely=0.5, anchor=CENTER)
 img_path=""
 def openFile():
     global img_path
-
+    img_path = filedialog.askopenfilename(title=" Open Text File", filetypes=[("Image Files","*.jpg *.gif *.jpg *.png *.jpeg")])
+    print(img_path)
+    im = Image.open(img_path)
+    img = ImageTk.PhotoImage(im)
+    label_image["image"] = img
+    img.close()
 
 def rotateImage():
     global img_path
-
+    im = Image.open(img_path)
+    img = ImageTk.PhotoImage(im.rotate(180))
+    label_image["image"] = img
+    print(img_path)
+    img.close()
    
 btn=Button(root,text="Open Image",font= ("Times New Roman0", 12),bg="grey",fg="white", command=openFile, relief=SOLID, padx=15, pady=10)
 btn.place(relx=0.5,rely=0.1,anchor=CENTER)
